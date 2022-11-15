88fa56b22163aa7305186baf928a63aa55349035
NeaCS <naldi.castro@gmail.com>
2022-11-15 18:54:01 -0300

    react bootstrap y material ui

---
+++ src/components/Bulma.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+function Card() {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-4by3">
+                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image" />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-left">
+                        <figure className="image is-48x48">
+                            <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
+                        </figure>
+                    </div>
+                    <div className="media-content">
+                        <p className="title is-4">John Smith</p>
+                        <p className="subtitle is-6">@johnsmith</p>
+                    </div>
+                </div>
+
+                <div className="content">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                    <a href="#">#css</a> <a href="#">#responsive</a>
+                    <br />
+                    <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                </div>
+            </div>
+            <footer class="card-footer">
+                <a href="#" class="card-footer-item">Save</a>
+                <a href="#" class="card-footer-item">Edit</a>
+                <a href="#" class="card-footer-item">Delete</a>
+            </footer>
+        </div>
+    );
+}
+
+export default function Bulma() {
+    return (
+        <>
+            <h2>Bootstrap</h2>
+            <Card />
+        </>
+    );
+}
---
+++ src/components/ReactBootstrap.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
+export default function ReactBootstrap() {
+    return (
+        <>
+            <h2>React Bootstrap</h2>
+        </>
+    );
+}
