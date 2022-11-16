076a652a42da442d101f6593075f0ecd0df20cd7
NeaCS <naldi.castro@gmail.com>
2022-11-16 12:02:27 -0300

    ultimo commit de frameworks css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react'
 /* import Bootstrap from "./components/Bootstrap" */
 /* import Bulma from "./components/Bulma" */
-import ReactBootstrap from './components/ReactBootstrap';
+/* import ReactBootstrap from './components/ReactBootstrap'; */
+import MaterialIU from './components/MaterialIU';
+
 
 
 
 
 function App() {
   return (
-    <div>
+    <div style={{marginTop:"7%"}}>
       <h1>Frameworks CSS con React</h1>
       {/* <Bootstrap/> */}
     {/*   <Bulma/> */}
-      <ReactBootstrap/>
+    {/*   <ReactBootstrap/> */}
+    <MaterialIU/>
     </div>
   );
 }
---
+++ src/components/MaterialIU.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MediaControlCard from "./MaterialCard"
+import DrawerUI from "./DrawerUI"
+
+export default function MaterialIU() {
+    return (
+        <>
+        <h2>Componente con Material UI</h2>
+        <MediaControlCard/>
+        <DrawerUI/>
+        </>
+    )
+}
